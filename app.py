1c1916607ca6746e62ac0c6d741e2e8dd8139f68
Kendall Reid <kendall.reid@wsu.edu>
2025-10-29 12:48:30 -0700

    fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "<h1>￿ Hello from Flask CI/CD Demo v1!</h1>"
+    return "<h1>￿ Hello from Flask CI/CD Demo v1!</h1>" \
         "<p>Automatic Build → Test → Deploy via GitHub Actions.</p>"
 
 
